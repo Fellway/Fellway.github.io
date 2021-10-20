f816b1fb4747d74cc7ef96b70df4bd8fa05345c5
Fellway <mateusz.skrzypczyk1@hotmail.com>
2021-10-20 13:24:29 +0200

    [LAB-2] Added function to draw player

--- lab2/script.js
+++ lab2/script.js
@@ -1,12 +1,29 @@
 var canvasWidth = 800;
 var canvasHeight = 600;
+/** @type {CanvasRenderingContext2D} */
 var context;
 
+const player = {
+    height: 20,
+    width: 100,
+    position: {
+        x: canvasWidth / 2 - 50,
+        y: canvasHeight - 60
+    }
+}
+
 function initGameArea(width, height) {
-     /** @type {CanvasRenderingContext2D} */
      context = document.getElementById("gameArea").getContext("2d");
      context.canvas.width = width;
      context.canvas.height = height;
 }
 
+function drawPlayerRectangle(width, height, color) {
+    context.fillStyle = color;
+    context.beginPath();
+    context.fillRect(player.position.x, player.position.y, width, height);
+    context.stroke(); 
+}
+
 initGameArea(canvasWidth, canvasHeight);
+drawPlayerRectangle(player.width, player.height, '#f44336');
